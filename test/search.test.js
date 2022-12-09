41d68e8612ae527e25198d02869e6974a4ed1a5d
pacheco94 <pgeorgealberto@gmail.com>
2022-12-09 21:26:43 +0200

    agregando la carpeta de test

---
+++ test/search.test.js
@@ -0,0 +1,26 @@
+const { expect } = require('chai');
+
+const SearchAdd = artifacts.require('SearchAdd');
+
+contract('Probando el contrato', function ([owner]){
+    let value = 17;
+    let vector = [1,2,3,4,5,7,8,9];
+
+ beforeEach(async () => {
+     
+     this.instance = await SearchAdd.new({from: owner})
+ });
+     
+ //probando la funcion setValue and getNumber
+ it('Should equal 17', async () => {
+    await this.instance.setValue([2,3,5,7,8,1,9,4], 17);
+    expect(await this.instance.getNumber(),value,'Value it not to expeted!');
+ });
+
+ //probando la funcion getVector
+ it('Should vector equal', async () => {
+     let _vector = await this.getVector.call();
+     expect(vector, _vector,'Vector its not to expected!');
+ });
+
+});
